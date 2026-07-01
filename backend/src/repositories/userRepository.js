4117cea0e1d0464b5450a931afaa9f66085acd94
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-07-01 11:28:17 +0530

    Added CRUD Repository.

---
+++ backend/src/repositories/userRepository.js
@@ -0,0 +1,18 @@
+import crudRepository from './crudRepository.js';
+import User from './schema/user.js';
+
+const userRepository = {
+    ...crudRepository(User),
+
+    getUserByEmail: async function (email) {
+        const user = await User.findOne({ email });
+        return user;
+    },
+
+    getUserByName: async function (name) {
+        const user = await User.findOne({ username: name });
+        return user;
+    }
+}
+
+export default userRepository;
