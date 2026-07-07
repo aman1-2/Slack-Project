0d2df46ee1123ce38e8bfc10e21a2065afb9198c
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-07-07 17:23:27 +0530

    Added the mailConfig file and tested it.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
 import apiRouter from './routes/index.js';
+import mailer from './config/mailConfig.js';
 
 
 const app = express();
@@ -18,7 +19,16 @@ app.get('/ping', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log('Started the server at Port:', PORT);
   connectDB();
+
+  const mailResponse = await mailer.sendMail({
+    from: 'amanpratapsinghh12@gmail.com',
+    to: 'amanpratapsinghh12@gmail.com',
+    subject: "Welcome mail",
+    text: "Welcome to the application"
+  });
+
+  console.log(mailResponse);
 });
