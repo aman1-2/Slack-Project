757d85e7dac35ad10541bd7fb4b68ff15f4677d2
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-07-10 18:29:26 +0530

    Added the slack color inside theme property of index.css.

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -3,7 +3,7 @@ import { SignupCard } from '@/components/organism/Auth/SignupCard.jsx';
 export const Auth = () => {
     return (
         <div
-            className="h-screen flex items-center justify-center bg-[#5c3B58]"
+            className="h-screen flex items-center justify-center bg-slack"
         >
             <div
                 className="mid:h-auto md:w-[420px]"
