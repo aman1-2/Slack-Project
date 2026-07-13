2b4ce4e834c6a45e53b334b8259294db05c02c8f
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-07-13 15:40:13 +0530

    Added the Logout functionality with Auth state clean up.

--- frontend/src/components/atoms/UserButton/UserButton.jsx
+++ frontend/src/components/atoms/UserButton/UserButton.jsx
@@ -1,11 +1,21 @@
 import { LogOutIcon, SettingsIcon } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import useAuth from '@/hooks/context/useAuth';
 
 export const UserButton = () => {
-    const { auth } = useAuth();
+    const { auth, logout } = useAuth();
+    const navigate = useNavigate();
+
+    async function logoutHandler() {
+        await logout();
+        toast.success('Successfully log out');
+        navigate('/auth/signin');
+    }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger className='outline-none relative'>
@@ -20,7 +30,7 @@ export const UserButton = () => {
                     Settings
                 </DropdownMenuItem>
 
-                <DropdownMenuItem>
+                <DropdownMenuItem onClick = {logoutHandler}>
                     <LogOutIcon className="size-4 mr-2 h-10"></LogOutIcon>
                     Logout
                 </DropdownMenuItem>
