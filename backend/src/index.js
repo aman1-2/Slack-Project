4cbb3fc6ebd3019d086b42be6e8edd7348e1b6f1
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2026-07-20 22:43:05 +0530

    Fixed the CORS issue for web-socket-io.

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,11 @@ import { verifyEmailController } from './controllers/userController.js';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
 
 app.use(cors());
 
